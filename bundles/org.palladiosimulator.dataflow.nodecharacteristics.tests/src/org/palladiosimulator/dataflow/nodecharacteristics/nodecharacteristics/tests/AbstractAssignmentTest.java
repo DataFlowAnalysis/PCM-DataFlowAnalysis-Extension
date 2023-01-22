a01853e3502b4cf4111f94ff2225695798acefd2
Felix Schwickerath <felix.schwickerath@t-online.de>
2023-01-22 19:30:48 +0100

    Add edit and editor projects

---
+++ bundles/org.palladiosimulator.dataflow.nodecharacteristics.tests/src/org/palladiosimulator/dataflow/nodecharacteristics/nodecharacteristics/tests/AbstractAssignmentTest.java
@@ -0,0 +1,55 @@
+/**
+ */
+package org.palladiosimulator.dataflow.nodecharacteristics.nodecharacteristics.tests;
+
+import junit.framework.TestCase;
+
+import org.palladiosimulator.dataflow.nodecharacteristics.nodecharacteristics.AbstractAssignment;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Abstract Assignment</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public abstract class AbstractAssignmentTest extends TestCase {
+
+	/**
+	 * The fixture for this Abstract Assignment test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected AbstractAssignment fixture = null;
+
+	/**
+	 * Constructs a new Abstract Assignment test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public AbstractAssignmentTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this Abstract Assignment test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(AbstractAssignment fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this Abstract Assignment test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected AbstractAssignment getFixture() {
+		return fixture;
+	}
+
+} //AbstractAssignmentTest
---
+++ bundles/org.palladiosimulator.dataflow.nodecharacteristics.tests/src/org/palladiosimulator/dataflow/nodecharacteristics/nodecharacteristics/tests/NodeCharacteristicsAllTests.java
@@ -0,0 +1,46 @@
+/**
+ */
+package org.palladiosimulator.dataflow.nodecharacteristics.nodecharacteristics.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test suite for the '<em><b>NodeCharacteristics</b></em>' model.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class NodeCharacteristicsAllTests extends TestSuite {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(suite());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static Test suite() {
+		TestSuite suite = new NodeCharacteristicsAllTests("NodeCharacteristics Tests");
+		return suite;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public NodeCharacteristicsAllTests(String name) {
+		super(name);
+	}
+
+} //NodeCharacteristicsAllTests
