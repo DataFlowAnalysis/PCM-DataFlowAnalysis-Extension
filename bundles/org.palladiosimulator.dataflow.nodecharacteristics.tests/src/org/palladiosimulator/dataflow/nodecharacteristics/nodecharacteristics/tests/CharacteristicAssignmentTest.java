678d1f0c80c71c1d7b589859c230c6285061cbf8
Felix Schwickerath <felix.schwickerath@t-online.de>
2023-01-23 09:00:09 +0100

    Add correct editor hierarchy

---
+++ bundles/org.palladiosimulator.dataflow.nodecharacteristics.tests/src/org/palladiosimulator/dataflow/nodecharacteristics/nodecharacteristics/tests/CharacteristicAssignmentTest.java
@@ -0,0 +1,89 @@
+/**
+ */
+package org.palladiosimulator.dataflow.nodecharacteristics.nodecharacteristics.tests;
+
+import junit.framework.TestCase;
+
+import junit.textui.TestRunner;
+
+import org.palladiosimulator.dataflow.nodecharacteristics.nodecharacteristics.CharacteristicAssignment;
+import org.palladiosimulator.dataflow.nodecharacteristics.nodecharacteristics.NodeCharacteristicsFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Characteristic Assignment</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class CharacteristicAssignmentTest extends TestCase {
+
+	/**
+	 * The fixture for this Characteristic Assignment test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected CharacteristicAssignment fixture = null;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(CharacteristicAssignmentTest.class);
+	}
+
+	/**
+	 * Constructs a new Characteristic Assignment test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public CharacteristicAssignmentTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this Characteristic Assignment test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(CharacteristicAssignment fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this Characteristic Assignment test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected CharacteristicAssignment getFixture() {
+		return fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(NodeCharacteristicsFactory.eINSTANCE.createCharacteristicAssignment());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //CharacteristicAssignmentTest
---
+++ bundles/org.palladiosimulator.dataflow.nodecharacteristics.tests/src/org/palladiosimulator/dataflow/nodecharacteristics/nodecharacteristics/tests/NodeCharacteristicAssignmentTest.java
@@ -0,0 +1,89 @@
+/**
+ */
+package org.palladiosimulator.dataflow.nodecharacteristics.nodecharacteristics.tests;
+
+import junit.framework.TestCase;
+
+import junit.textui.TestRunner;
+
+import org.palladiosimulator.dataflow.nodecharacteristics.nodecharacteristics.NodeCharacteristicAssignment;
+import org.palladiosimulator.dataflow.nodecharacteristics.nodecharacteristics.NodeCharacteristicsFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Node Characteristic Assignment</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class NodeCharacteristicAssignmentTest extends TestCase {
+
+	/**
+	 * The fixture for this Node Characteristic Assignment test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected NodeCharacteristicAssignment fixture = null;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(NodeCharacteristicAssignmentTest.class);
+	}
+
+	/**
+	 * Constructs a new Node Characteristic Assignment test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public NodeCharacteristicAssignmentTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this Node Characteristic Assignment test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(NodeCharacteristicAssignment fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this Node Characteristic Assignment test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected NodeCharacteristicAssignment getFixture() {
+		return fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(NodeCharacteristicsFactory.eINSTANCE.createNodeCharacteristicAssignment());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //NodeCharacteristicAssignmentTest
