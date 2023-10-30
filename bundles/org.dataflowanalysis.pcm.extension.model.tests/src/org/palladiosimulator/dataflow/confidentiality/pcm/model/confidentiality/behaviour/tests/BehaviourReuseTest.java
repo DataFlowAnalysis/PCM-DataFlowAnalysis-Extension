f13c64cfa5404e9ea259adfba5ef692daaf33e95
Felix Schwickerath <felix.schwickerath@student.kit.edu>
2023-10-30 16:22:29 +0100

    Rename bundles and fix dependency errors

---
+++ bundles/org.dataflowanalysis.pcm.extension.model.tests/src/org/palladiosimulator/dataflow/confidentiality/pcm/model/confidentiality/behaviour/tests/BehaviourReuseTest.java
@@ -0,0 +1,89 @@
+/**
+ */
+package org.palladiosimulator.dataflow.confidentiality.pcm.model.confidentiality.behaviour.tests;
+
+import junit.framework.TestCase;
+
+import junit.textui.TestRunner;
+
+import org.palladiosimulator.dataflow.confidentiality.pcm.model.confidentiality.behaviour.BehaviourFactory;
+import org.palladiosimulator.dataflow.confidentiality.pcm.model.confidentiality.behaviour.BehaviourReuse;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Reuse</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class BehaviourReuseTest extends TestCase {
+
+	/**
+	 * The fixture for this Reuse test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected BehaviourReuse fixture = null;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(BehaviourReuseTest.class);
+	}
+
+	/**
+	 * Constructs a new Reuse test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public BehaviourReuseTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this Reuse test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(BehaviourReuse fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this Reuse test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected BehaviourReuse getFixture() {
+		return fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(BehaviourFactory.eINSTANCE.createBehaviourReuse());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //BehaviourReuseTest
---
+++ bundles/org.dataflowanalysis.pcm.extension.model.tests/src/org/palladiosimulator/dataflow/confidentiality/pcm/model/confidentiality/expression/tests/VariableCharacterizationLhsTest.java
@@ -0,0 +1,55 @@
+/**
+ */
+package org.palladiosimulator.dataflow.confidentiality.pcm.model.confidentiality.expression.tests;
+
+import junit.framework.TestCase;
+
+import org.palladiosimulator.dataflow.confidentiality.pcm.model.confidentiality.expression.VariableCharacterizationLhs;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Variable Characterization Lhs</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public abstract class VariableCharacterizationLhsTest extends TestCase {
+
+	/**
+	 * The fixture for this Variable Characterization Lhs test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected VariableCharacterizationLhs fixture = null;
+
+	/**
+	 * Constructs a new Variable Characterization Lhs test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public VariableCharacterizationLhsTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this Variable Characterization Lhs test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(VariableCharacterizationLhs fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this Variable Characterization Lhs test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected VariableCharacterizationLhs getFixture() {
+		return fixture;
+	}
+
+} //VariableCharacterizationLhsTest
